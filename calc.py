5a928e60a6df86cab718a16ae83a3b275a9a7572
EShau00 <EShau00@gmail.com>
2020-07-09 17:43:09 +0000

    It begins.

--- calc.py
+++ calc.py
@@ -1,4 +1,84 @@
-def add(x, y):
-    return x + y
+import re
 
-print(add(2, 3))
+string = "5 + 2 / 4 * 4 - 2 + 6 / 53"
+
+def eval(string):
+    '''
+    MDAS:   Multiplication and Division
+            Addition and Subtraction
+    '''
+
+    def perform_operation(parsed, operation):
+        operation_index = parsed.index(operation)
+        parsed.pop(operation_index)
+        a = float(parsed.pop(operation_index-1))
+        b = float(parsed.pop(operation_index-1))
+        if operation == '*':
+            parsed.insert(operation_index-1, str(a * b))
+        elif operation == '/':
+            parsed.insert(operation_index-1, str(a / b))
+        elif operation == '+':
+            parsed.insert(operation_index-1, str(a + b))
+        elif operation == '-':
+            parsed.insert(operation_index-1, str(a - b))
+
+    parsed = re.split("([\*/\+-])", string)
+    print(parsed)
+    parsed = [entry.strip() for entry in parsed]
+    print(parsed)
+
+    while len(parsed) != 1:
+
+        # Performs multiplication and division operations
+        while '*' in parsed or '/' in parsed:
+
+            print(parsed)
+
+            # We can guarentee division exists
+            if '*' not in parsed:
+                perform_operation(parsed, '/')
+            
+            # We can guarentee multiplication exists
+            elif '/' not in parsed:
+                perform_operation(parsed, '*')
+            
+            # We can guarentee both exist
+
+            # Left to Right
+            elif parsed.index('*') < parsed.index('/'):
+                perform_operation(parsed, '*')
+            
+            # Left to Right
+            elif parsed.index('/') < parsed.index('*'):
+                 perform_operation(parsed, '/')
+            
+            # We don't need an else statement because these 5 statements cover all cases
+
+        # Performs addition and subtraction operations
+        while '+' in parsed or '-' in parsed:
+
+            print(parsed)
+
+            # We can guarentee division exists
+            if '+' not in parsed:
+                perform_operation(parsed, '-')
+            
+            # We can guarentee multiplication exists
+            elif '-' not in parsed:
+                perform_operation(parsed, '+')
+            
+            # We can guarentee both exist
+
+            # Left to Right
+            elif parsed.index('+') < parsed.index('-'):
+                perform_operation(parsed, '+')
+            
+            # Left to Right
+            elif parsed.index('-') < parsed.index('+'):
+                 perform_operation(parsed, '-')
+            
+            # We don't need an else statement because these 5 statements cover all cases
+
+    return(float(parsed.pop()))
+
+print(eval(string))
